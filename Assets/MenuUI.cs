da8c2a1c0c16c1c172a144adfbb59bc8667d11ad
kolejkar <karolpomarzynski@gmail.com>
2019-11-17 20:05:25 +0100

    Signed-off-by: kolejkar <karolpomarzynski@gmail.com>

---
+++ Assets/MenuUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+
+    public static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: "+this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exting ...");
+        Application.Quit();
+    }
+
+    public void ChangeScene()
+    {
+
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/player_move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player_move : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(new Vector2(-200 * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector2(200 * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(new Vector2(0, 500 * Time.deltaTime));
+        }
+
+        rb.AddForce(new Vector2(0, 150 * Time.deltaTime));
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D: "+ col.collider.name);
+        if (col.collider.name == "Sciana")
+            LoadScene();
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Game over", LoadSceneMode.Single);
+    }
+}
